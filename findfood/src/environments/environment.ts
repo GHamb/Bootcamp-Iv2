3ecbbc901beedaef718fc81531e3ad95159b936d
Gustavo Hamburg <Gustavo.hamburg@outlook.com>
2021-07-28 21:10:14 -0300

    terceiro dia conexão fire base

--- findfood/src/environments/environment.ts
+++ findfood/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBHLRv8ClY1T8Xuc-iK6OonY2qpWnBafyM",
+    authDomain: "findfood-iv2.firebaseapp.com",
+    projectId: "findfood-iv2",
+    storageBucket: "findfood-iv2.appspot.com",
+    messagingSenderId: "381900446717",
+    appId: "1:381900446717:web:b0ee25997296d416a91794"
+  }
 };
 
 /*
